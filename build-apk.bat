1ef6ca8b19c56e8fd4612a28437bdd407e1637da
peculiaro10-oss <peculiaro10@gmail.com>
2026-09-06 11:20:54 +0100

    Update build-apk.bat

--- build-apk.bat
+++ build-apk.bat
@@ -1,6 +1,11 @@
 @echo off
+
+set "JAVA_HOME=C:\Program Files\Eclipse Adoptium\jdk-21.0.12.101-hotspot"
+set "PATH=%JAVA_HOME%\bin;%PATH%"
+
 cd /d "%~dp0android"
 
+call gradlew.bat --stop
 call gradlew.bat assembleDebug
 
 if errorlevel 1 (
@@ -14,4 +19,5 @@ echo.
 echo BUILD SUCCESSFUL
 echo APK:
 echo %~dp0android\app\build\outputs\apk\debug\app-debug.apk
+
 pause
